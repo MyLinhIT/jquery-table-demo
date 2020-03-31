6a3a802b5231f33b1d5176b351f5aba70b11a977
MyLinhIT <43976718+MyLinhIT@users.noreply.github.com>
2020-03-31 10:11:03 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 (function ($) {
-  let current_page = 1
-  let TOTAL_PAGE = 0
   $.fn.tablePlugin = function () {
     let $div = $('.table-jquery')
     $div.append(`<table class="table table-bordered table-striped custom-table"><thead><tr></tr></thead><tbody></tbody></table>`)
     $div.append(`<div class="wrap-pagination"></div>`)
     let $table = $('table tbody')
     let $thead = $("table thead tr")
+    let current_page = 1
 
     const checkButtonClick = {
       create: [],
@@ -99,6 +98,7 @@
         item.employee_salary = item.employee_salary.trim().replace(/^0/, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
         return item
       })
+
 
       $.each(dataFormat, function (index, item) {
         let tr = `<tr id=${item.id === "N/A" ? "" : item.id} ${item.id === "N/A" ? "add-row" : ""}>`
@@ -273,6 +273,70 @@
         });
         bindDataForm(values);
       }
+    }
+
+    // handle pagination
+    handleChangePagination = function (curentPage) {
+      current_page = curentPage
+      $.fn.handleLoadingPagination()
+    }
+
+    handlePreviousPagination = function () {
+      if (current_page === 1)
+        return;
+      current_page = current_page - 1
+      $.fn.handleLoadingPagination()
+    }
+
+    handleNextPagination = function () {
+      if (current_page >= Math.ceil(dataSource.data.length / dataSource.pagination.pageSize))
+        return;
+      current_page += 1
+      $.fn.handleLoadingPagination()
+    }
+
+    handleFirstPagination = function () {
+      current_page = 1
+      $.fn.handleLoadingPagination()
+    }
+
+    handleLastPagination = function () {
+      current_page = Math.ceil(dataSource.data.length / dataSource.pagination.pageSize)
+      $.fn.handleLoadingPagination()
+    }
+
+    // Ref: https://www.yogihosting.com/jquery-pagination/
+    $.fn.createPagination = function () {
+      let totalPage = Math.ceil(dataSource.data.length / dataSource.pagination.pageSize)
+      let span = ''
+      span += `<span class="item-pagination ${current_page === 1 ? "disable" : ""}"" onClick="handleFirstPagination()">&#10092;&#10092;</span>`
+      span += `<span class="item-pagination ${current_page === 1 ? "disable" : ""}" onClick="handlePreviousPagination()">&#10092;</span>`
+
+      if (current_page - 3 > 1) {
+        span += `<span class="item-pagination ${current_page === 1 ? "active" : ""}" onClick="handleChangePagination(${1})">${1}</span>`
+        span += `<span class="item-more" style="display: ${current_page === 1 ? "none" : "inline-block"}" onClick="handlePreviousPagination()">•••</span>`
+      }
+
+      for (let i = current_page - 3; i <= current_page; i++) {
+        if (i >= 1) {
+          span += `<span class="item-pagination ${current_page === i ? "active" : ""}" onClick="handleChangePagination(${i})">${i}</span>`
+        }
+      }
+
+      for (let i = current_page + 1; i <= current_page + 3; i++) {
+        if (i <= totalPage) {
+          span += `<span class="item-pagination ${current_page === i ? "active" : ""}" onClick="handleChangePagination(${i})">${i}</span>`
+        }
+      }
+
+      if (current_page + 3 < totalPage) {
+        span += `<span class="item-more" style="display: ${current_page === totalPage ? "none" : "inline-block"}" onClick="handleNextPagination()">•••</span>`
+        span += `<span class="item-pagination ${current_page === totalPage ? "active" : ""}" onClick="handleChangePagination(${totalPage})">${totalPage}</span>`
+      }
+
+      span += `<span class="item-pagination ${current_page >= totalPage ? "disable" : ""}" onClick="handleNextPagination()">&#10093;</span>`
+      span += `<span class="item-pagination ${current_page >= totalPage ? "disable" : ""}" onClick="handleLastPagination()">&#10093;&#10093;</span>`
+      $(".wrap-pagination").append(span)
     }
 
     // handle event button click
